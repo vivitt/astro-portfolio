b92173f6cc36e75243c8469803caab3c445c9922
vivitt <viviviyanez@gmail.com>
2025-04-07 18:00:57 +0200

    add goat counter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import partytown from "@astrojs/partytown";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://viviyanez.dev",
-  integrations: [icon()],
+  integrations: [icon(), partytown()],
 });
